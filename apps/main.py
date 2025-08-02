361d18a67ff5e4499f8ae00549db17d201219f66
muhammadfarras <maruffarras@gmail.com>
2025-08-02 17:45:12 +0700

    edit catatan query chroma

--- apps/main.py
+++ apps/main.py
@@ -1,25 +1,30 @@
 import chromadb
 import os
-import uuid
-
-client = chromadb.Client()
-
-# Membua sebuah collection
-collection = client.create_collection("peraturan_rumah")
+from pprint import pp
 
 path_peraturan = os.path.abspath("data/peraturan_rumah.txt")
 
 with open (path_peraturan, mode="r", encoding="utf-8") as f:
     kebijakan = f.read().splitlines()
-    
+
+# Object Client
+client = chromadb.Client()
+
+# Membuat sebuah collection
+collection = client.create_collection("peraturan_rumah")
+
+# To generate ID per chuking data
+idGenerate = [f"id{a}" for a in range (1, len(kebijakan)+1)]
+
 collection.add(
-    ids=[str(uuid.uuid4()) for a in kebijakan],
+    ids=idGenerate,
     documents=kebijakan,
     metadatas=[{"kebijakan":a} for a in range(len(kebijakan))]
 )
 
 pertanyaan=[
-        "Siapa yang tidak sekolah ?"
+        "Siapa yang tidak sekolah ?",
+        "Apa tugas hilyah"
     ]
 
 query_response = collection.query(
@@ -27,7 +32,6 @@ query_response = collection.query(
     n_results=2
 )
 
-for number,a in enumerate(query_response["documents"]):
-    print(f"Pertanyaan ({number}) : {pertanyaan[number]}")
-    print(f"Jawaban : ","****\r\n****".join(a))
-
+for number,a in enumerate(query_response["documents"]): # type: ignore
+    pp(f"Pertanyaan ({number}) : {pertanyaan[number]}")
+    pp(a)
